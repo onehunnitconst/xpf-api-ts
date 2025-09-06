ca51c0ec69c311a69c579043aa3d3ca3d4850e95
onehunnitconst <skygana@gmail.com>
2025-09-06 16:11:00 +0900

    constants 수정

--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,12 @@ import * as path from 'path';
 
 dotenv.config({ path: path.resolve('.env') });
 
-const { DATABASE_URL } = process.env;
+const { DATABASE_URL, REDIS_HOST, REDIS_PORT } = process.env;
 
 export default abstract class Constants {
   static databaseUrl = DATABASE_URL;
+  static redisHost = REDIS_HOST;
+  static redisPort = REDIS_PORT;
   static jwtPrivateKeyPath = path.join(
     __dirname,
     '..',
